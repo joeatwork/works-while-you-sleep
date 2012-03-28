59422b2b113dcbc2f4b58bf3d72bad56945d8d2f
Joe Bowers <joerbowers@gmail.com>
2012-03-27 19:42:15 -0700

    one day's worth of stuff. Character walks and moves.

--- site/js/bounds.js
+++ site/js/bounds.js
@@ -1,47 +1,143 @@
 window.Bounds = (function() {
     "use strict";
 
+    /**
+     * A fast integer set, intended for dense collections
+     * of ints close to zero.
+     */
+    var BitMap = function() {
+        this.int32s = [];
+    };
+
+    BitMap.create = function() {
+        return new BitMap();
+    };
+
+    BitMap.prototype = {
+
+        setBit : function(index, val) {
+            var intIndex = Math.floor(index / 32);
+            var bitIndex = index % 32;
+
+            var oldMask = this.int32s[intIndex] || 0;
+            var newBit = 1 << bitIndex;
+
+            var newMask = val ?
+                oldMask | newBit :
+                oldMask & ~newBit;
+
+            this.int32s[intIndex] = newMask;
+        },
+
+        getBit : function(index, val) {
+            var intIndex = Math.floor(index / 32);
+            var bitIndex = index % 32;
+
+            var int32 = this.int32s[intIndex];
+            var bit = 1 << bitIndex;
+
+            // Sign extension means we can't
+            // just return bit >> index
+            return (int32 & bit) ? 1 : 0
+        },
+
+        // TODO DELETE OR JUSTIFY
+        // TODO: There has *got* to be a faster
+        //       (memcpy-ish) way to do this.
+        setBlock : function(low, high, val) {
+            for(var i=low; i<=high; i++) {
+                this.setBit(i, val);
+            }
+        },
+
+        cost : function() {
+            return this.int32s.length;
+        }
+
+    };// BitMap
+
+    /*******************************************************************/
+
+    var canvasToBitMap = function(canvas) {
+        var gfx = canvas.getContext("2d");
+        var imageData = gfx.getImageData(0,0,
+                                         canvas.width,
+                                         canvas.height);
+        var pixels = imageData.data;
+        var bitMapLength = Math.ceil(pixels.length / 4);
+        var bitMap = new BitMap();
+        for(var bit_ix = 0; bit_ix < bitMapLength ; bit_ix++) {
+            var alpha = pixels[ (bit_ix * 4) + 3 ];
+            bitMap.setBit(bit_ix, alpha);
+        }
+
+        return bitMap;
+    };
+
+    // CONVENTION - we represent bounds as an image, with
+    // alpha == 0 at out of bounds and alpha > 0 in bounds.
     var Bounds = function(image) {
-	var bounds = this;
+        var bounds = this;
 
-	bounds.width = image.width;
-	bounds.height = image.height;
-	bounds.alphas = null;
+        bounds.width = image.width;
+        bounds.height = image.height;
 
-	var boundsCanvas = document.createElement("canvas");
-	boundsCanvas.width = bounds.width;
-	boundsCanvas.height = bounds.height;
+        var boundsCanvas = document.createElement("canvas");
+        boundsCanvas.width = bounds.width;
+        boundsCanvas.height = bounds.height;
 
-	var gfx = boundsCanvas.getContext("2d");
-	gfx.drawImage(image, 0, 0);
+        var gfx = boundsCanvas.getContext("2d");
+        gfx.drawImage(image, 0, 0);
 
-	setTimeout(function() {
-	    var gfx = boundsCanvas.getContext("2d");
-	    var imageData = gfx.getImageData(0,0, 
-					     boundsCanvas.width,
-					     boundsCanvas.height);
-
-	    var pixels = imageData.data;
-	    var alphas = new Array(pixels.length / 4);
-	    for(var pixel_ix = 0; pixel_ix < alphas.length; pixel_ix++) {
-		alphas[pixel_ix] = pixels[ (pixel_ix * 4) + 3 ];
-	    }
-
-	    bounds.alphas = alphas;
-	}, 0);
+        // Delay because drawImage is QUEUED, but (probably)
+        // not actually RUN.
+        setTimeout(function() {
+            bounds.bitMap = canvasToBitMap(boundsCanvas);
+        }, 0);
     };
     
+    Bounds.BitMap = BitMap;
+
     Bounds.prototype = {
-	ready : function() {
-	    return this.alphas;
-	},
-	check : function(x,y) {
-	    return this.alphas[x + (y * this.width) ];
-	}
-    };
+
+        ready : function() {
+            return this.bitMap;
+        },
+
+        check : function(x,y) {
+            var checkBit = x + (y * this.width);
+            return this.bitMap.getBit(checkBit);
+        },
+
+        checkClear : function(x, y, width, height) {
+            return (this.check(x,y) && 
+                    this.check(x + width, y) &&
+                    this.check(x, y + height) &&
+                    this.check(x + width, y + height));
+        },
+
+        // TODO DELETE OR JUSTIFY
+        blockRect : function(x, y, width, height) {
+            this._markRect(x, y, width, height, 0);
+        },
+
+        // TODO DELETE OR JUSTIFY
+        clearRect : function(x, y, width, height) {
+            this._markRect(x, y, width, height, 1);
+        },
+
+        // TODO DELETE OR JUSTIFY
+        _markRect : function(x, y, width, height, mark) {
+            for(var iY = y; iY <= y + height; iY++) {
+                var lowCol = x + (iY * this.width);
+                var highCol = lowCol + width;
+                this.bitMap.setBlock(lowCol, highCol, mark);
+            }
+        }
+    };// Bounds.prototype
 
     Bounds.create = function(image) {
-	return new Bounds(image);
+        return new Bounds(image);
     }
 
     return Bounds;
--- site/js/character.js
+++ site/js/character.js
@@ -4,10 +4,11 @@
 window.Character = (function() {
     "use strict";
 
-    var Character = function() {}
 
     /**************************************************/
-    Character.Costume = (function() {
+    // a Costume manages drawing and animating a character
+    // to a graphics context.
+    var Costume = (function() {
 
         var NORTH = 0;
         var EAST = 1;
@@ -23,10 +24,8 @@ window.Character = (function() {
             return orientation;
         };
 
-        var ANIMATION_SPEED_MS = 500;
+        var ANIMATION_SPEED_MS = 250;
 
-        // a Costume manages drawing and animating a character
-        // to a graphics context.
         var Costume = function(image, spriteWidth, spriteHeight) {
             this.image = image;
             this.spriteWidth = spriteWidth;
@@ -38,34 +37,42 @@ window.Character = (function() {
             return new Costume(image, spriteWidth, spriteHeight);
         };
 
-        // gfx - a graphics context
-        // posX, posY - point in gfx coordinates to draw the upper left corner
-        //    of the character
-        // dx, dy - direction of motion of character in the X and Y directions,
-        //    as -1, 0, or 1
-        // time - in millisecond ticks, from some zero.
-        Costume.prototype.draw = function(gfx,
-                                          posX, posY,
-                                          dx, dy,
-                                          time) {
-            if(!time) time = 0;
+        Costume.prototype = {
 
-            var orientation = SOUTH;
-            var cellOffset = 0;
+            // gfx - a graphics context
+            // posX, posY - point in gfx coordinates to draw the upper left corner
+            //    of the character
+            // dx, dy - direction of motion of character in the X and Y directions,
+            //    as -1, 0, or 1
+            // time - in millisecond ticks, from some zero.
+            draw : function(gfx, posX, posY, dx, dy, time) {
+                if(!time) time = 0;
 
-            if(dx || dy) {
-                orientation = dirToOrientation(dx, dy);
-                var animTime = Math.floor(time/ANIMATION_SPEED_MS);
-                cellOffset = 1 + (animTime % 2);
+                var orientation = SOUTH;
+                var cellOffset = 0;
+
+                if(dx || dy) {
+                    orientation = dirToOrientation(dx, dy);
+                    var animTime = Math.floor(time/ANIMATION_SPEED_MS);
+                    cellOffset = 1 + (animTime % 2);
+                }
+
+                var cellY = this.spriteHeight * orientation;
+                var cellX = this.spriteWidth * cellOffset;
+
+                gfx.drawImage(this.image,
+                              cellX, cellY, this.spriteWidth, this.spriteHeight,
+                              posX, posY, this.spriteWidth, this.spriteHeight);
+            },
+
+            width : function() {
+                return this.spriteWidth;
+            },
+
+            height : function() {
+                return this.spriteHeight;
             }
-
-            var cellY = this.spriteHeight * orientation;
-            var cellX = this.spriteWidth * cellOffset;
-
-            gfx.drawImage(this.image,
-                          cellX, cellY, this.spriteWidth, this.spriteHeight,
-                          posX, posY, this.spriteWidth, this.spriteHeight);
-        };
+        }; // Costume.prototype
 
 
         return Costume;
@@ -74,19 +81,88 @@ window.Character = (function() {
 
     /**************************************************************/
 
+    // Characters are constructed with
+    //  - a Costume, to represent themselves on gfx contexts
+    var Character = function(costume) {
+        this.costume = costume;
+        this.xSpeed = 0;
+        this.ySpeed = 0;
+    }
+
+    Character.Costume = Costume;
+
     Character.create = function(costume) {
-        var ret = new Character();
-        ret.costume = costume;
-        return ret;
+        return new Character(costume);
     };
 
     Character.prototype = {
+
+        //  - bounds - has a check(x,y) method
+        //  - a Footprint rectangle {x, y, width, height } for collision detection
+        //       where x,y are offset from costume's top left corner
+        setBounds : function(bounds, footX, footY, footWidth, footHeight) {
+            this.bounds = bounds;
+            this.footX = footX || 0;
+            this.footY = footY || 0;
+            this.footWidth = footWidth || this.costume.width();
+            this.footHeight = footHeight || this.costume.height();
+        },
+
+        setPosition : function(xPosition, yPosition) {
+            this.xPosition = xPosition;
+            this.yPosition = yPosition;
+        },
+
+        // Speed should be expressed in pixels-per second
+        setSpeed : function(xSpeed, ySpeed) {
+            this.xSpeed = xSpeed;
+            this.ySpeed = ySpeed;
+        },
 
         // Advance this character's state through time. Likely VERY SPECIAL.
         // Callers should promise that if called with time === x, ALL future
         // calls will have time > x
         move : function(time) {
+            if(! this.now) {
+                this.now = time;
+                return;
+            }
+            // ELSE, if time is valid
+
+            var deltaT = time - this.now;
+
+            var dX = this.xSpeed * (deltaT/1000);
+            var dY = this.ySpeed * (deltaT/1000);
+
+            var targetX = this.xPosition + dX;
+            var targetY = this.yPosition + dY;
+
+            if(this.bounds) {
+                var targetFootX = Math.floor(this.footX + targetX);
+                var targetFootY = Math.floor(this.footY + targetY);
+		var targetWidth = this.footWidth;
+		var targetHeight = this.footHeight;
+
+                var isClear =
+                    this.bounds.checkClear(targetFootX,
+                                           targetFootY,
+                                           targetWidth,
+                                           targetHeight);
+
+                if(isClear) {
+                    this.xPosition = targetX;
+                    this.yPosition = targetY;
+                }
+		else {
+		    var oldXSpeed = this.xSpeed;
+		    this.xSpeed = -this.ySpeed;
+		    this.ySpeed = oldXSpeed;
+		}
+            }
+
             this.now = time;
+
+            // CHECK this.bounds
         },
 
         // Draw the RIGHT THING on the canvas, for the time
@@ -94,8 +170,9 @@ window.Character = (function() {
         // Does not handle Z-ordering.
         draw : function(gfx) {
             this.costume.draw(gfx,
-                              this.xPosition, this.yPosition,
-                              this.xSpeed, this,ySpeed,
+                              Math.floor(this.xPosition),
+                              Math.floor(this.yPosition),
+                              this.xSpeed, this.ySpeed,
                               this.now);
         },
     };
